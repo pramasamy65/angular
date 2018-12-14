9ffa76eff029bca05a30bbdcc6903e066b0833b7
pramasamy65 <prakashkec@gmail.com>
2018-12-14 06:57:02 +0530

    Adding Dec 14 2018 work

---
+++ test-app/src/app/server/server.component.ts
@@ -0,0 +1,20 @@
+import { Component }from '@angular/core';
+
+
+@Component({
+selector : 'app-server',
+templateUrl : './server.component.html',
+styleUrls: []
+})
+
+
+export class ServerComponent { 
+
+    serverId : number = 100;
+    serverStatus : string = "Offline";
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
+
+}
