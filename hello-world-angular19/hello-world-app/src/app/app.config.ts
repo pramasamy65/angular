2cc41b2c57951dc47504c6391fac5eb2c39f9bcb
pramasamy65 <32251153+pramasamy65@users.noreply.github.com>
2026-01-22 15:49:29 +0000

    Sample Hello world example with Latest Angular

---
+++ hello-world-angular19/hello-world-app/src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay()),
+    
+  ]
+};
